958aa6fdb2564d74fd01d0b5098ab298779f47a6
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:36:40 +0300

    24

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-a = '5px';
-b = '6px';
-alert(parseInt(a) + parseInt(b));
-a = '5.5px';
-b = '6.25px';
-alert(parseFloat(a) + parseFloat(b));
-a = '5.5px';
-b = '6.25px';
-alert(parseFloat(a) + parseFloat(b) + 'px');
+a = 5;
+b = 11;
+alert(String(a) + String(b));
+a = 137387341123213;
+alert(String(a).length);
+a = 2313132312132; // 13
+b = 31246786695; // 11
+alert(String(a).length + String(b).length); // должно быть 24
