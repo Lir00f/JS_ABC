8b4c9545d0828212daa07e5f32f2e9afd9fabc1c
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:38:20 +0300

    25

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-a = 5;
-b = 11;
-alert(String(a) + String(b));
-a = 137387341123213;
-alert(String(a).length);
-a = 2313132312132; // 13
-b = 31246786695; // 11
-alert(String(a).length + String(b).length); // должно быть 24
+alert(true + 3);
+alert(true + true);
+alert(true - true);
+alert(true + false);
+alert('1' + true);
+alert( String(true) + 1 );
+alert( String(true) + Number(true) );
