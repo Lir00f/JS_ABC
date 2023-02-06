74a4f0d673b550a9d4437853550451db51b9a9b7
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:04:54 +0300

    16

--- script.js
+++ script.js
@@ -2,11 +2,5 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-str = "!!!"
-alert(str);
-a="java";
-b="script";
-alert(a + b);
-c="hello";
-d="world";
-alert(c + ' ' + d);
+a = "PhantomAssasin";
+alert(a.length);
