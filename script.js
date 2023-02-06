743f6adbea7fb5aa86c81b94555eda57aac7b25c
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:29:45 +0300

    22

--- script.js
+++ script.js
@@ -2,31 +2,12 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-a = '5' + '2';
-alert(a);
-a = '5' + 2;
-alert(a);
-a = 5 + '2';
-alert(a);
-a = 5 + 2;
-alert(a);
-a = '5' * '2';
-alert(a);
-a = '5' - '2';
-alert(a);
-a = '5' / '2';
-alert(a);
-a = '5' % '2';
-alert(a);
-a = '5s' * '2';
-alert(a);
-a = '5s' + '2';
-alert(a);
-a = (-'5') + (-'2');
-alert(a);
-a = '5' * 1 + '2' * 1;
-alert(a);
-a = '5' * '1' + '2' * '1';
-alert(a);
-a = '' + 3 + 1;
-alert(a);
+a = '10';
+b = '20';
+alert(Number(a) + Number(b));
+alert( Number('2') + Number('3') );
+alert( 2 + Number('3') );
+alert( '2' + Number('3') );
+a = '2';
+b = '3';
+alert(+a + +b);
