6f8e7a86dd42644a81897513f625e39594675bd3
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:02:40 +0300

    15

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-str = "Nikolaenko Elvin"
+str = "!!!"
 alert(str);
+a="java";
+b="script";
+alert(a + b);
+c="hello";
+d="world";
+alert(c + ' ' + d);
