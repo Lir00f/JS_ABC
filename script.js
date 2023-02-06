7857927bd6c1006bc2e112619322a6a4c1df1b7c
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 21:15:46 +0300

    18

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
 
 // Присваиваем переменным значения:
-let str1 = 'xxx';
-let str2 = 'yyy';
-let txt  = `aaa  ${str1}  bbb  ${str2}  ccc`;
-alert(txt);
-
-// 2
-str = `a
-b
-c`;
-alert(str);
+alert(a);
+a = null;
+alert(a);
+a = true;
+alert(a);
+a = false;
+alert(a);
+c = 'bebra'
+d = 'gaming'
+alert(c * d);
+a = 10;
+b = -10;
+c = 0;
+alert(a / c);
+alert(b / c);
