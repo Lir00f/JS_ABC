34247eebd0246b1c8497559636f6f6b111e33463
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 16:59:37 +0300

    41

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let array = [1, 2, 3, 4, 5, 6];
-console.log(array.length);
+let arr = ['a', 'b', 'c'];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
 
-let array1 = [1, 2, 3, 4, 5, 6];
-console.log(array1[array1.length - 1]) 
+let array1 = [1, 2, 3];
+array1[0]++;
+array1[1]++;
+array1[2]++;
+console.log(array1);
+
+let array2 = [1, 2, 3, 4];
+array2[0] += 3;
+array2[1] += 3;
+array2[2] += 3;
+array2[3] += 3;
+console.log(array2)
