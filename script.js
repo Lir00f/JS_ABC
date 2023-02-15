1781aaf324ded3ffe976041a112ebceeec7e7064
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 17:03:31 +0300

    45

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let array = [];
-array.push(1);
-array.push(2);
-array.push(3);
-console.log(array);
-// 2
-let array1 = [1, 2, 3];
-array1.push(4);
-array1.push(5);
-console.log(array1);
+let array = ['a', 'b', 'c'];
+let key = 2;
+alert(array[key]);
+
+let array1 = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2;
+alert(array1[key1] + array1[key2]);
