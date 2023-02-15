09b0cb4dfe20d4dc736f3da5678c4e67af04bea6
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 16:53:39 +0300

    35

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-document.write('Привет');
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ', num1 + num2); 
 
-document.write('<p style="font-style: italic;">Привет</p>');
+let a = 1;
+let b = 2;
+console.log(a + b);
 
-let str = 'Мир!';
-document.write('<p style="font-style: italic;"> str </p>');
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
 
-document.write('1<br>');
-document.write('2<br>');
-document.write('3<br>');
-document.write('4<br>');
-document.write('5<br>');
+let num3 = '123';
+console.log(num3[0]);
+
+let a1 = 0;
+console.log(++a1);
+
+let num4 = '123';
+console.log(num4.length);
+
+let a2 = 24 / 60 / 60;
+console.log(a2.toFixed(4));
+
+let num5 = 123;
+let str = String(num5);
+console.log(str.length);
+
+let num6 = 123;
+let str1 = String(num6);
+console.log(str1[str1.length-1]);
+
+let a3 = '123';
+let b3 = '456';
+let s = Number(a3) + String(b3);
+console.log(Number(s));
