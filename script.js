137fa4924f313d1016dcd46ce8e7f2d1c86bd6e4
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 17:04:55 +0300

    47

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let array = ['a', 'b', 'c', 'd', 'e'];
+let array = [1, 2, 3, 4, 5];
 
-delete array[1];
-delete array[3];
+alert(array[array.length-1]);
 
-alert(array.length); 
-alert(array); 
+
+let array1 = [1, 2, 3, 4, 5];
+
+alert(array1[0] + array1[1] + array1[2] + array1[3] + array1[4]); 
+
+
+let array2 = [1, 2, 3, 4, 5];
+
+alert(array2.length);
