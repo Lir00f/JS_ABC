3014e27185e17419cb979ac202efd3e8e1ed6921
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 17:11:36 +0300

    52

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let obj = {year: 2004, month: 11, day: 13};
-alert(obj.year + " " + obj.month +" "+ obj.day);
+let obj = {
+    // Обязательно
+	'1a': 1, 
+	'b2': 2,
+    // Обязательно
+	'с-с': 3,
+    // Обязательно
+	'd 4': 4,
+	'e5': 5
+};
+
+let obj1 = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+alert(obj1['1a']);
+alert(obj1.b2);
+alert(obj1['c-c']);
+alert(obj1['d 4']);
+alert(obj1.e5);
