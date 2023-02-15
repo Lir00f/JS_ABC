28a26177357a804f2399a56ddddc18216c1f3a32
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 16:54:18 +0300

    37

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-let num1 = 1;
-let num2 = 2;
-console.log('сумма: ', num1 + num2); 
+alert(60*24);
 
-let a = 1;
-let b = 2;
-console.log(a + b);
+alert(60*24*30);
 
-let num = '123';
-let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
-console.log(sum);
+alert(60*365);
 
-let num3 = '123';
-console.log(num3[0]);
-
-let a1 = 0;
-console.log(++a1);
-
-let num4 = '123';
-console.log(num4.length);
-
-let a2 = 24 / 60 / 60;
-console.log(a2.toFixed(4));
-
-let num5 = 123;
-let str = String(num5);
-console.log(str.length);
-
-let num6 = 123;
-let str1 = String(num6);
-console.log(str1[str1.length-1]);
-
-let a3 = '123';
-let b3 = '456';
-let s = Number(a3) + String(b3);
-console.log(Number(s));
+alert(2**30);
