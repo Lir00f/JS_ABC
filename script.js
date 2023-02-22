53f4e9c373a5648bceaeaa35b260c4ffa07b6906
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-22 23:04:39 +0300

    70

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const arr = ["a", "b", "c"];
-arr[1] = "!";
-console.log(arr);
-arr = [1, 2, 3];
-console.log(arr);
-arr = ["a", "b", "c"];
-console.log(arr);
+const arr = [1, 2, 3, 4, 5];
+const res = arr[1] + arr[2];
+
+console.log(res);
