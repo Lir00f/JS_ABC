51114ad414f8c21272d3c32dd6521ed21ed8ca32
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-01 19:40:18 +0300

    109

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-let num = '123033'; 
-let str = String(num);
+let num2 = 123033; // берем в кавычки, чтобы обращаться к цифрам
+let str3 = String(num2);
 
-let sum1 = str[0] + str[1] + str[2];
-let sum2 = str[3] + str[4] + str[5];
+let sum1 = +str3[0] + +str3[1] + +str3[2];
+let sum2 = +str3[3] + +str3[4] + +str3[5];
 
 if (sum1 == sum2) {
-	console.log('=');
+	document.write('суммы равны');
 } else {
-	console.log('!=');
+	document.write('суммы не равны');
 }
