b3831a25c393be1d6425d5b06da54f55945368ca
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-01 19:30:06 +0300

    84

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-if (num == 0 || num > 1 && num < 5 ) {
+let num = 3;
+		
+if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {
 	console.log('+++');
 } else {
 	console.log('---');
