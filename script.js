9e04f8876054bee6f1c40d43fffdb3eda2bbecde
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-01 19:41:54 +0300

    112

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 "use strict";
 
-for (let i = 1; i <= 100; i++) {
-	console.log(i);
-}
-
-for (let i = 11; i <= 33; i++) {
-	console.log(i);
-}
-
-for (let i = 0; i <= 100; i += 2) {
-	console.log(i);
-}
-
-for (let i = 1; i <= 99; i += 2) {
-	console.log(i);
-}
-
-for (let i = 100; i > 0; i--) {
-	console.log(i);
+let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+for (let element of array) {
+	console.log(element);
 }
