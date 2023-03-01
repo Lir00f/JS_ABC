c0f137b81d67590ca98009ff3bd42d956ccf813f
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-01 19:40:49 +0300

    110

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 "use strict";
 
-let num2 = 123033; // берем в кавычки, чтобы обращаться к цифрам
-let str3 = String(num2);
-
-let sum1 = +str3[0] + +str3[1] + +str3[2];
-let sum2 = +str3[3] + +str3[4] + +str3[5];
-
-if (sum1 == sum2) {
-	document.write('суммы равны');
-} else {
-	document.write('суммы не равны');
-}
+// Тут просто введение в циклы в главе
