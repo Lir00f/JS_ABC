ed60221ddc3f92346cc634803d41e57cd098d135
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-01 19:39:48 +0300

    108

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 "use strict";
 
-let a = 10;
+let num = '123033'; 
+let str = String(num);
 
-if (a % 2 == 0) {
-	document.write('четное', '<br>');
+let sum1 = str[0] + str[1] + str[2];
+let sum2 = str[3] + str[4] + str[5];
+
+if (sum1 == sum2) {
+	console.log('=');
 } else {
-	document.write('нечетное', '<br>');
-}
-
-let b = 9;
-
-if (b % 3 == 0) {
-	document.write('делится');
-} else {
-	document.write('не делится');
+	console.log('!=');
 }
