2e2d7db821ba3e24af499806731123baa12aca17
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-08 16:44:22 +0300

    118

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-let res = 0;
-for (let i = 2; i <= 100; i++) {
-    if(i % 2 == 0){
-	res += i;
-    }
+let array = [2, 5, 9, 3, 1, 4];
+let result = 0;
+for (let element of array) {
+	result += element;
 }
-console.log(res);
+console.log(result)
 
-let res1 = 0;
-for (let i = 1; i <= 99; i++) {
-    if(i % 2 != 0){
-	res1 += i;
+let array1 = [2, 5, 9, 3, 1, 4];
+let result1 = 0;
+for (let element of array1) {
+    if(element % 2 == 0){
+	result1 += element;
     }
 }
-console.log(res1);
+console.log(result1)
 
-let res2 = 1;
-for (let i = 1; i <= 20; i++) {
-    if(i % 2 != 0){
-	res2 *= i;
-    }
+let array2 = [2, 5, 9, 3, 1, 4];
+let result2 = 0;
+for (let element of array2) {
+	result2 += element**2;
 }
-console.log(res2);
+console.log(result2)
+
+let array3 = [2, 5, 9, 3, 1, 4];
+let result3 = 1;
+for (let element of array3) {
+	result3 *= element;
+}
+console.log(result3)
