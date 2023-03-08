a2a82d424b506ad058b286022f56617ed823b786
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-08 16:45:59 +0300

    119

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-let array = [2, 5, 9, 3, 1, 4];
-let result = 0;
-for (let element of array) {
-	result += element;
-}
-console.log(result)
+"use strict";
 
-let array1 = [2, 5, 9, 3, 1, 4];
-let result1 = 0;
-for (let element of array1) {
-    if(element % 2 == 0){
-	result1 += element;
-    }
-}
-console.log(result1)
 
-let array2 = [2, 5, 9, 3, 1, 4];
-let result2 = 0;
-for (let element of array2) {
-	result2 += element**2;
-}
-console.log(result2)
+let str = '';
 
-let array3 = [2, 5, 9, 3, 1, 4];
-let result3 = 1;
-for (let element of array3) {
-	result3 *= element;
+for (let i = 1; i <= 5; i++) {
+	str += '-';
 }
-console.log(result3)
+
+console.log(str);
+
+
+str = '';
+
+for (let i = 1; i <= 9; i++) {
+	str += i;
+}
+
+console.log(str);
+
+
+str = '';
+
+for (let i = 9; i >= 1; i--) {
+	str += i;
+}
+
+console.log(str);
+
+
+str = '';
+
+for (let i = 1; i <= 9; i++) {
+	str += i + '-';
+}
+
+console.log(str);
