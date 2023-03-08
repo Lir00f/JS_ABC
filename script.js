c060f36abdab71667136c52a025155c98475fc4c
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-08 16:40:53 +0300

    114

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let obj = {x: 1, y: 2, z: 3};
-for (let key in obj) {
-	console.log(key); 
-    console.log(obj[key]); 
+let i = 1;
+while (i <= 100) {
+	console.log(i);
+	i++;
+}
+// 2
+let i1 = 11;
+while (i1 <= 33) {
+	console.log(i1);
+	i1++;
+}
+let i2 = 3;
+while (i2 <= 1000){
+    i2 = i2 * 3;
+console.log(i2)
 }
