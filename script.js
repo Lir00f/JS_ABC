47c6220b635c6b16286215ceffda763cc1c4efaf
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-03-08 16:54:30 +0300

    130

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 "use strict";
 
 //////
-let obj1 = {x: 1, y: 2, z: 3};
+let array = ['a', 'b', 'c', 'd', 'e'];
+let flaag = false;
 
-for (let key in obj1) {
-   obj1[key] = obj1[key] ** 2;
-};
+for (let element of array) {
+   if (element == 'c') {
+	  flaag = true;
+	  break;
+   }
+}
 
-console.log(obj1)
+if (flaag === true) {
+   alert('+++');
+} else {
+   alert('---');
+}
 
 //////
-let obj2 = {x: 1, y: 2, z: 3};
+let num = 4;
+let flaag2 = true;
+let i = 2;
 
-for (let key in obj2) {
-   obj2[key] += 1
+while (i < num) {
+   if (num % i == 0) {
+	  flaag2 = !flaag2;
+	  break;
+
+   } else {
+	  i += 1;
+   };
 };
 
-console.log(obj2)
+if (flaag2) {
+   alert(`Число ${num} является простым`)
+} else {
+   alert(`Число ${num} не является простым`)
+}
