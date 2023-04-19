affe46e89f8c3451869b45e0e3f901f1329cbef5
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-04-19 21:36:50 +0300

    133

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-"use strict";
-
-let str = 'Osman ti bidlo?';
-let arr = str.split('') 
-let count = {};         
-let result = 0 
-
-for (let item of arr) {
-   if (count[item] === undefined) {
-	  count[item] = 1;
-   } else {
-	  count[item]++;
-   }
+let arr1 = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr1.length - 1; i++) {
+    console.log(arr1[i+1]);
 }
 
-for (let item in count) {
-   if(count[item] > 1) {
-	  result += count[item]
-   };
-};
 
-document.write(result);
+
+let arr2 = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr2.length - 1; i++) {
+    console.log(arr2[i] + arr2[i+1]);
+}
+
+
+
+let arr3 = [1, 2, 3, 4, 5];
+for (let i = 2; i <= arr3.length - 1; i++) {
+    console.log( arr3[i-1] + " " + arr3[i-2]);
+}
+
+
+
+let arr4 = [1, 2, 3, 4, 5];
+for (let i = 2; i <= arr4.length - 1; i++) {
+    console.log( arr4[i] + arr4[i-1] + arr4[i-2]);
+}
+
+
+
+let arr5 = [1, 2, 3, 4, 5];
+for (let i = 1; i < arr5.length - 1; i++) {
+    console.log(arr5[i-1] + arr5[i] + arr5[i+1]);
+}
