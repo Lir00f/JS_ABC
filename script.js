94cdf368fc5894a83815cecb5dab15a45043f093
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-04-19 21:37:47 +0300

    135

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let arr = [10, 20, 30, 40, 21, 32, 51];
+let obj = {a: 10, b: 20, c: 30, d: 40, e: 50};
 let sum = 0;
-for (let i of arr) {
-    if (String(i)[0] == '1' || String(i)[0] == '2') {
-        sum = sum + Number(i);
-    }
+
+for (let elem in obj) {
+	if (String(obj[elem])[0] === '1' || String(obj[elem])[0] === '2') {
+		sum += obj[elem];
+	}
 }
+
 console.log(sum);
