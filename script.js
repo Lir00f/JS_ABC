49181630ac8f5c518b98483b8557336fad7d3716
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-18 22:31:50 +0300

    243

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-button1 = document.querySelector("#button1");
-button2 = document.querySelector("#button2");
-button3 = document.querySelector("#button3");
-button1.addEventListener("click", function () {
-    document.write(1)
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+button1.addEventListener('click', function() {
+	console.log('1');
 });
-button2.addEventListener("click", function () {
-    document.write(2)
+button2.addEventListener('click', function() {
+	console.log('2');
 });
-button3.addEventListener("click", function () {
-    document.write(3)
+button3.addEventListener('click', function() {
+	console.log('3');
 });
