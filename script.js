44da1b63aeb667172b6625bc0e6207ff74859016
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-06-18 22:25:51 +0300

    218

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let options = {
-    color: "red",
     width: 400,
     height: 500,
 };
-let {color: c, width: w, height: h} = options;
-console.log(c)
-console.log(w)
-console.log(h)
+let { color = 'red', width, height} = options;
+console.log(color);
+console.log(width);
+console.log(height);
